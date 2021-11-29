2cc945bbf30c8011264f812cfc98ee0deae4d7d7
chdupire <cdupire@gmail.com>
2021-11-29 16:37:15 +0100

    get the book's title

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'http://books.toscrape.com'
+url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
 
 response = requests.get(url)
+results = {}
 
 if response.ok:
-    print(response.text)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    title = soup.findAll('title')
+print(title)
 
+str = str(title)
 
+for i in range(len(str) + 1):
+    while str[i] != '>':
+        i = i + 1
